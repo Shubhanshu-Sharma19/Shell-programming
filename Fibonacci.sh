7dbfb6d2cbcc118f44eb01aab7fdda903c7249bd
Shubhanshu-Sharma19 <24mca010@ncuindia.edu>
2025-02-17 15:56:31 +0530

    Fibonacci.sh

---
+++ Fibonacci.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+# Shubhanshu Sharma , 24mca010
+echo -n "Enter the number of terms:
+read n
+a=0
+b=1 echo "Fibonacci Series:"
+echo -n "Sa $b for (( 1=2; i‹n; i# ))
+do c=$(a + b)) echo an "$c a=$b
+b=$c
