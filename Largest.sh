328b4edba744f3cd337348483f9005822697daeb
Shubhanshu-Sharma19 <24mca010@ncuindia.edu>
2025-02-17 15:57:26 +0530

    Largest.sh

---
+++ Largest.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+# Shubhanshu Sharma, 24mca010
+echo -n "Enter first number:
+read a
+echo -n "Enter second number:
+read b
+echo -n "Enter third number:
+read c if IL $a -st $b 86 $a -gt $c 11; then
+echo "Largest number is: Sa" elif [[ $b -gt $a ss $b -st $c ]]; then
+echo "Largest number is: $b' else echo "Largest number is: $c"
